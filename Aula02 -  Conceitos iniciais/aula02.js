e42d3c4a5e5578b8f3d80fa257c3f64e9d5eab51
Luis Felipe Ribeiro <44618496+felipert2@users.noreply.github.com>
2023-10-18 12:47:37 -0300

    e

--- Aula02 -  Conceitos iniciais/aula02.js
+++ Aula02 -  Conceitos iniciais/aula02.js
@@ -1,3 +1,8 @@
-console.log("Carregando...");
-console.log("Uma mensagem qualquer!");
+let numero = prompt("Digite um numero de 1 a 5");
 
+if (numero <= 5){
+    alert("Seu numero é " + numero)
+}
+else {
+ alert("Numero Invalido")
+}
